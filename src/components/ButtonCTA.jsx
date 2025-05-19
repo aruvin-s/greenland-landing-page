93fd21a3dd8d26b45e1a3a52354eb0f92338ae0a
Alvin Wiraprathama <wiraprathamaalvin@gmail.com>
2025-05-19 13:32:04 +0700

    chores: finish hero sections

---
+++ src/components/ButtonCTA.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ButtonCTA = () => {
+  return (
+    <a
+    href="https://wa.me/6282144036169"
+    target="_blank"
+    rel="noopener noreferrer"
+    className=
+    'flex justify-center items-center font-jakarta font-bold text-white bg-white/20 w-[260px] h-[60px] outline-2 rounded'>
+        Reservasi Sekarang
+    </a>
+  );
+};
+
+export default ButtonCTA;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Navbar from '.././components/Navbar.jsx'
+import ButtonCTA from '.././components/ButtonCTA.jsx'
 import bghero from '.././assets/bghero.png'
 
 const Hero = () => {
@@ -14,9 +15,10 @@ const Hero = () => {
             <h1 className='font-mirage font-bold text-white text-[64px]/18 pb-1'>
                 Harmoni di Antara <br/> Malang dan Batu
             </h1>
-            <p className='font-jakarta text-white text-[24px]/18'>
+            <p className='font-jakarta text-white text-[20px]/18 pb-4'>
                 Nikmati ketenangan dan keindahan menginap ditengah alam hanya sekejap dari pusat kota
             </p>
+            <ButtonCTA/>
         </div>
     </div>
   )
