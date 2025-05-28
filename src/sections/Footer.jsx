ab5758c5993bfbadd7fd4764a8104ad2c0309741
Alvin Wiraprathama <wiraprathamaalvin@gmail.com>
2025-05-28 11:35:34 +0700

    fix: fix footer and testimony section responsive

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,73 +6,83 @@ import image from '.././assets/footer.png'
 
 const Footer = () => {
   return (
-    <div>
-       <div className="bg-accent flex py-20">
-            <div className="flex justify-center gap-20 w-full px-[104px] font-jakarta text-secondary">
-                <div className="flex flex-col max-w-sm">
-                  <img src={Logo} alt="Villa Booking Logo"/>
-                  <img src={image} alt="Footer Image"/>
-                </div>
-                <div className="flex flex-col max-w-sm  gap-3">
-                  <p className='font-bold text-lg'>Menu</p>
-                  <div className='h-[1px] w-full bg-secondary'></div>
-                  <a>About</a>
-                  <a>Stay</a>
-                  <a>FAQ</a>
-                  <a>Review</a>
-                </div>
-                <div className="flex flex-col max-w-sm  gap-3">
-                  <p className='font-bold text-lg'>Kontak dan Reservasi</p>
-                  <div className='h-[1px] w-full bg-secondary'></div>
-                  <div className='flex gap-1 items-center'>
-                    <PhoneIcon size={20} />
-                    <a>082144036169</a>
-                  </div>
-                  <div className='flex gap-1 items-center'>
-                    <WhatsappLogoIcon size={20} />
-                    <a>082133523123</a>
-                  </div>
-                  <div className='flex gap-1 items-center'>
-                    <EnvelopeIcon size={20} />
-                    <a>mlg.greenlandglamping@gmail.com</a>
-                  </div>
-                  <div className='flex gap-1 items-center'>
-                    <InstagramLogoIcon size={20} />
-                    <a>greenlandmalang</a>
-                  </div>
-                  <div className='flex gap-1 items-center'>
-                    <GlobeIcon size={20} />
-                    <a>greenlandmalang.com</a>
-                  </div>
-                </div>
-                <div className='flex flex-col gap-5'>
-                  <div className="flex flex-col max-w-sm gap-3">
-                    <p className='font-bold text-lg'>Alamat</p>
-                    <div className='h-[1px] w-full bg-secondary'></div>
-                    <div className='flex items-start gap-2'>
-                      <MapPinIcon size={20} className='mt-[6px]' />
-                      <div className='text-secondary leading-8'>
-                        <p>Lasah, Tawangargo,</p>
-                        <p>Karangploso</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col max-w-sm gap-3">
-                    <p className='font-bold text-lg'>Jam Operasional</p>
-                    <div className='h-[1px] w-full bg-secondary'></div>
-                    <div className='flex items-start gap-2'>
-                      <ClockIcon size={20} className='mt-[6px]' />
-                      <div className='text-secondary leading-8'>
-                        <p>Senin - Minggu</p>
-                        <p>08.00 am - 21.00 pm</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </div>
+    <div className="bg-accent py-20">
+      <div className="flex flex-col lg:flex-row justify-center gap-12 md:gap-20 w-full px-4 sm:px-8 md:px-[104px] font-jakarta text-secondary mx-auto items-center lg:items-start">
+        <div className="flex flex-col flex-shrink-0 w-full sm:w-lg">
+            <img 
+              src={Logo} 
+              alt="Villa Booking Logo" 
+              className="mb-6 max-w-full h-auto object-contain" 
+            />
+            <img 
+              src={image} 
+              alt="Footer Image" 
+              className="max-w-full h-auto object-cover rounded-md" 
+            />
+          </div>
+
+        <div className="flex flex-col max-w-full mb-8 md:mb-0 gap-3 text-center lg:text-start">
+          <p className="font-bold text-lg">Menu</p>
+          <div className="h-[1px] bg-secondary mb-3"></div>
+          <a href="#" className="hover:underline transition">About</a>
+          <a href="#" className="hover:underline transition">Stay</a>
+          <a href="#" className="hover:underline transition">FAQ</a>
+          <a href="#" className="hover:underline transition">Review</a>
         </div>
+
+        <div className="flex flex-col mb-8 md:mb-0 gap-3 items-center lg:items-start">
+          <p className="font-bold text-lg">Kontak dan Reservasi</p>
+          <div className="h-[1px] w-full bg-secondary mb-3"></div>
+          <div className="flex gap-2 items-center">
+            <PhoneIcon size={20} />
+            <a href="tel:082144036169" className="hover:underline transition">082144036169</a>
+          </div>
+          <div className="flex gap-2 items-center">
+            <WhatsappLogoIcon size={20} />
+            <a href="https://wa.me/082133523123" className="hover:underline transition">082133523123</a>
+          </div>
+          <div className="flex gap-2 items-center">
+            <EnvelopeIcon size={20} />
+            <a href="mailto:mlg.greenlandglamping@gmail.com" className="hover:underline transition">mlg.greenlandglamping@gmail.com</a>
+          </div>
+          <div className="flex gap-2 items-center">
+            <InstagramLogoIcon size={20} />
+            <a href="https://instagram.com/greenlandmalang" className="hover:underline transition">@greenlandmalang</a>
+          </div>
+          <div className="flex gap-2 items-center">
+            <GlobeIcon size={20} />
+            <a href="https://greenlandmalang.com" className="hover:underline transition">greenlandmalang.com</a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-3 items-center lg:items-start">
+            <p className="font-bold text-lg">Alamat</p>
+            <div className="h-[1px] w-full bg-secondary mb-3"></div>
+            <div className="flex items-start gap-2">
+              <MapPinIcon size={20} className="mt-[6px]" />
+              <div className="text-secondary leading-8">
+                <p>Lasah, Tawangargo,</p>
+                <p>Karangploso</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center lg:items-start">
+            <p className="font-bold text-lg">Jam Operasional</p>
+            <div className="h-[1px] w-full bg-secondary mb-3"></div>
+            <div className="flex items-start gap-2">
+              <ClockIcon size={20} className="mt-[6px]" />
+              <div className="text-secondary leading-8">
+                <p>Senin - Minggu</p>
+                <p>08.00 am - 21.00 pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Footer
--- src/sections/Testimony.jsx
+++ src/sections/Testimony.jsx
@@ -5,33 +5,34 @@ import { WhatsappLogoIcon , ArrowSquareRightIcon , ArrowSquareLeftIcon  } from '
 import testi1 from '.././assets/testimony1.png'
 import testi2 from '.././assets/testimony2.png'
 
-const Testimony= () => {
+const Testimony = () => {
   return (
     <div>
-        <div className="min-h-screen bg-primary flex flex-col justify-center items-center">
-            <div className="flex flex-col justify-center items-center gap-6 py-24">
-                <Chips variant="secondary" text="TESTIMONI"/>
-                <p className="font-mirage text-secondary font-bold text-4xl text-center">
-                    Bagaimana pengalaman mereka?
-                </p>
-                <div className='flex gap-10'>
-                  <TestimonyCard
-                  name="Octaviana"
-                  rating={5}
-                  image={testi1}
-                  message="Harga sangat terjangkau, fasilitas sangat lengkap dan bersih, pelayanan yang sangat ramah, pemandangan yang luar biasa, sangat cocok untuk keluarga"
-                  />
-                  <TestimonyCard
-                  name="Rina Sh"
-                  rating={5}
-                  image={testi2}
-                  message="Cuaca sejuk di pagi hari, pemandangan citylight keren di malam hari, cocok untuk memanjakan mata sambil quality time bersama orang tersayang"
-                  />
-                </div>
-            </div>
+      <div className="min-h-screen bg-primary flex flex-col justify-center items-center px-4 sm:px-8">
+        <div className="flex flex-col justify-center items-center gap-6 py-24 max-w-7xl mx-auto w-full">
+          <Chips variant="secondary" text="TESTIMONI" />
+          <p className="font-mirage text-secondary font-bold text-3xl sm:text-4xl text-center px-2 sm:px-0">
+            Bagaimana pengalaman mereka?
+          </p>
+          <div className="flex flex-col sm:flex-row sm:justify-center sm:gap-10 gap-8 w-full max-w-5xl">
+            <TestimonyCard
+              name="Octaviana"
+              rating={5}
+              image={testi1}
+              message="Harga sangat terjangkau, fasilitas sangat lengkap dan bersih, pelayanan yang sangat ramah, pemandangan yang luar biasa, sangat cocok untuk keluarga"
+            />
+            <TestimonyCard
+              name="Rina Sh"
+              rating={5}
+              image={testi2}
+              message="Cuaca sejuk di pagi hari, pemandangan citylight keren di malam hari, cocok untuk memanjakan mata sambil quality time bersama orang tersayang"
+            />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Testimony
